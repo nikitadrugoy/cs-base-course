ec2d6b1d0697d7d5fa131f741bc7a0dd9a388140
Nikita Sidorin <nikitadrugoy@gmail.com>
2021-02-27 22:35:27 +0300

    Init

---
+++ Utils/ConsoleHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Utils
+{
+    public static class ConsoleHelper
+    {
+        public static string Prompt(string message)
+        {
+            Console.Write($"{message}: ");
+            
+            return Console.ReadLine();
+        }
+        
+        public static void PrintInPosition(string message, int left, int top)
+        {
+            Console.SetCursorPosition(left, top);
+            Console.WriteLine(message);
+        }
+
+        public static void ExitByAnyKey()
+        {
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+        
+        public static void HideCursor()
+        {
+            Console.CursorVisible = false;
+        }
+        
+        public static void ShowCursor()
+        {
+            Console.CursorVisible = true;
+        }
+    }
+}
