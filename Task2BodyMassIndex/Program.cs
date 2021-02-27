479380377e4f6be96d589759521bcb1e201d7adc
Nikita Sidorin <nikitadrugoy@gmail.com>
2021-02-27 22:40:35 +0300

    Feat: Added task 2.

---
+++ Task2BodyMassIndex/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Utils;
+
+namespace Task2BodyMassIndex
+{
+    /// <summary>
+    /// Автор: Сидорин Никита
+    /// Курс: Основы языка C#
+    /// Урок: 1
+    /// Задача: 2
+    /// Условие: Ввести вес и рост человека.
+    /// Рассчитать и вывести индекс массы тела (ИМТ) по формуле I=m/(h*h);
+    /// где m — масса тела в килограммах, h — рост в метрах
+    /// </summary>
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var height = int.Parse(ConsoleHelper.Prompt("Enter your height (sm)"));
+            var weight = int.Parse(ConsoleHelper.Prompt("Enter your weight (kg)"));
+
+            var heightMeters = height / 100d;
+            var bodyMassIndex = weight / (heightMeters * heightMeters);
+            
+            Console.WriteLine($"Your body mass index is {bodyMassIndex:F1}.");
+            
+            ConsoleHelper.ExitByAnyKey();
+        }
+    }
+}
