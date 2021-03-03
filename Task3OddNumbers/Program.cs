25a77eea88d455fb41b165f0633e6a201d5d654f
Nikita Sidorin <nikitadrugoy@gmail.com>
2021-03-03 12:14:43 +0300

    Feat: Added task 3.

---
+++ Task3OddNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Utils;
+
+namespace Task3OddNumbers
+{
+    /// <summary>
+    /// Автор: Сидорин Никита
+    /// Курс: Основы языка C#
+    /// Урок: 2
+    /// Задача: 3
+    /// Условие: С клавиатуры вводятся числа, пока не будет введен 0.
+    /// Подсчитать сумму всех нечетных положительных чисел.
+    /// </summary>
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var oddNumbersCount = 0;
+            int currentNumber;
+
+            do
+            {
+                currentNumber = int.Parse(ConsoleHelper.Prompt("Enter number (0 to exit)"));
+
+                if (IsOdd(currentNumber))
+                {
+                    oddNumbersCount++;
+                } 
+            } while (currentNumber != 0);
+            
+            Console.WriteLine($"Odd numbers count: {oddNumbersCount}");
+        }
+
+        private static bool IsOdd(int value)
+        {
+            return value % 2 != 0;
+        }
+    }
+}
