35ae48fc961efaa2ede36b965e409bc09788aa6c
Nikita Sidorin <nikitadrugoy@gmail.com>
2021-03-03 12:35:56 +0300

    Ref: Added comment to task 2.

--- Task2NumberLength/Program.cs
+++ Task2NumberLength/Program.cs
@@ -3,6 +3,13 @@ using Utils;
 
 namespace Task2NumberLength
 {
+    /// <summary>
+    /// Автор: Сидорин Никита
+    /// Курс: Основы языка C#
+    /// Урок: 2
+    /// Задача: 2
+    /// Условие: Написать метод подсчета количества цифр числа.
+    /// </summary>
     internal class Program
     {
         public static void Main(string[] args)
