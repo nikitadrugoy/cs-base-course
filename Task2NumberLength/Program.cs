86b61dc96312777169155743e0a23643f1440747
Nikita Sidorin <nikitadrugoy@gmail.com>
2021-03-03 11:57:46 +0300

    Feat: Added task 2.

---
+++ Task2NumberLength/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Utils;
+
+namespace Task2NumberLength
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var number = ConsoleHelper.Prompt("Enter number");
+            
+            Console.WriteLine($"Number length is {number.Length}.");
+            
+            ConsoleHelper.ExitByAnyKey();
+        }
+    }
+}
